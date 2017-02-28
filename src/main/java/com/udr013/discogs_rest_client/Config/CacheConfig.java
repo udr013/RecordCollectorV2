814ce9a17c63af26cf43ad646d58797eea4732d8
udr013 <udr013@gmail.com>
2017-02-28 20:57:21 +0100

    cache configuration without  xml

--- src/main/java/com/udr013/discogs_rest_client/Config/CacheConfig.java
+++ src/main/java/com/udr013/discogs_rest_client/Config/CacheConfig.java
@@ -1,21 +1,34 @@
 package com.udr013.discogs_rest_client.Config;
 
 import net.sf.ehcache.config.CacheConfiguration;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CachingConfigurer;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.ehcache.EhCacheCacheManager;
+import org.springframework.cache.interceptor.CacheErrorHandler;
+import org.springframework.cache.interceptor.CacheResolver;
+import org.springframework.cache.interceptor.KeyGenerator;
+import org.springframework.cache.interceptor.SimpleCacheErrorHandler;
+import org.springframework.cache.interceptor.SimpleCacheResolver;
+import org.springframework.cache.interceptor.SimpleKeyGenerator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 
 @Configuration
 @EnableCaching
-public class CacheConfig {
+public class CacheConfig implements CachingConfigurer{
+
+	@Value("${cache.lifetime.seconds}")
+	private long cacheLifetimeSeconds;
 
 	@Bean(destroyMethod="shutdown")
 	public net.sf.ehcache.CacheManager ehCacheManager() {
 		CacheConfiguration cacheConfiguration = new CacheConfiguration();
 		cacheConfiguration.setName("Coll_Cache");
 		cacheConfiguration.setMaxEntriesLocalHeap(500);
-		cacheConfiguration.setTimeToLiveSeconds(10);
+		cacheConfiguration.setTimeToLiveSeconds(cacheLifetimeSeconds);
 
 
 		net.sf.ehcache.config.Configuration config = new net.sf.ehcache.config.Configuration();
@@ -24,4 +37,28 @@ public class CacheConfig {
 		return net.sf.ehcache.CacheManager.newInstance(config);
 	}
 
+
+	@Bean
+	@Override
+	public CacheManager cacheManager() {
+		return new EhCacheCacheManager(ehCacheManager()); //no need for ehcache.xml
+	}
+
+	@Bean
+	@Override
+	public KeyGenerator keyGenerator() {
+		return new SimpleKeyGenerator();
+	}
+
+	@Bean
+	@Override
+	public CacheResolver cacheResolver() {
+		return new SimpleCacheResolver(cacheManager());   //cacheManager cannot be null
+	}
+
+	@Bean
+	@Override
+	public CacheErrorHandler errorHandler() {
+		return new SimpleCacheErrorHandler();
+	}
 }
